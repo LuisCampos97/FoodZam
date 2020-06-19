624b10550ae4e1c85918e4912a083126f4a046fe
Luís Campos <luismmc1997@hotmail.com>
2020-06-19 16:54:38 +0100

    Implementação da previsão do fruto com base num input de uma imagem.

--- app/src/main/java/pt/ipleiria/estg/foodzam/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/foodzam/MainActivity.java
@@ -3,6 +3,17 @@ package pt.ipleiria.estg.foodzam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.request.model.PredictRequest;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.Model;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        ClarifaiClient client = new ClarifaiBuilder(getString(R.string.clarifai_api_key)).buildSync();
+
+        Model<Concept> generalModel = client.getDefaultModels().generalModel();
+
+        PredictRequest<Concept> request = generalModel.predict().withInputs(
+                ClarifaiInput.forImage("https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg")
+        );
+
+        List<ClarifaiOutput<Concept>> results = request.executeSync().get();
+
+        for(int i = 0; i < results.get(0).data().size(); i++) {
+            float prob = (results.get(0).data().get(0).value()) * 100;
+            System.out.println(results.get(0).data().get(i).name() + " - " + prob +"%");
+        }
     }
 }
