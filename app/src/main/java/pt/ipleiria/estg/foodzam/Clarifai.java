b7c62dae20976082baa29eaed269d07f8009ce9c
Luís Campos <luismmc1997@hotmail.com>
2020-06-21 19:47:55 +0100

    Previsão do fruto com base numa foto escolhida na galeria.

---
+++ app/src/main/java/pt/ipleiria/estg/foodzam/Clarifai.java
@@ -0,0 +1,41 @@
+package pt.ipleiria.estg.foodzam;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.request.model.PredictRequest;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.Model;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
+public class Clarifai extends AppCompatActivity {
+
+    private ClarifaiClient client;
+    private static Model<Concept> generalModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        client = new ClarifaiBuilder(getString(R.string.clarifai_api_key)).buildSync();
+        generalModel = client.getDefaultModels().generalModel();
+    }
+
+    public static List<Concept> predictImage(byte[] image) {
+        PredictRequest<Concept> request = generalModel.predict().withInputs(
+                ClarifaiInput.forImage(image)
+        );
+
+        List<ClarifaiOutput<Concept>> results = request.executeSync().get();
+        System.out.println(results);
+        return results.get(0).data();
+    }
+}
--- app/src/main/java/pt/ipleiria/estg/foodzam/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/foodzam/MainActivity.java
@@ -1,15 +1,31 @@
 package pt.ipleiria.estg.foodzam;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
 import clarifai2.api.request.model.PredictRequest;
+import clarifai2.dto.input.ClarifaiFileImage;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.model.Model;
 import clarifai2.dto.model.output.ClarifaiOutput;
@@ -17,27 +33,72 @@ import clarifai2.dto.prediction.Concept;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int GALLERY_REQUEST_CODE = 123;
+
+    //Views
+    private ImageView imageView;
+    private Button btnGaleria;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+        btnGaleria = findViewById(R.id.galeriaButton);
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        ClarifaiClient client = new ClarifaiBuilder(getString(R.string.clarifai_api_key)).buildSync();
 
-        Model<Concept> generalModel = client.getDefaultModels().generalModel();
 
-        PredictRequest<Concept> request = generalModel.predict().withInputs(
-                ClarifaiInput.forImage("https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg")
-        );
+        btnGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Pick an image"), GALLERY_REQUEST_CODE);
+            }
+        });
+    }
 
-        List<ClarifaiOutput<Concept>> results = request.executeSync().get();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imageData = data.getData();
+            imageView.setImageURI(imageData);
 
-        for(int i = 0; i < results.get(0).data().size(); i++) {
-            float prob = (results.get(0).data().get(0).value()) * 100;
-            System.out.println(results.get(0).data().get(i).name() + " - " + prob +"%");
+            ClarifaiClient client = new ClarifaiBuilder(getString(R.string.clarifai_api_key)).buildSync();
+            final Model<Concept> generalModel = client.getDefaultModels().generalModel();
+
+            PredictRequest<Concept> request = generalModel.predict().withInputs(
+                    ClarifaiInput.forImage(convertImageToByte(imageData))
+            );
+
+            List<ClarifaiOutput<Concept>> results = request.executeSync().get();
+            List<Concept> resulstsData = results.get(0).data();
+
+            for(int i = 0; i < resulstsData.size(); i++) {
+                float prob = (resulstsData.get(i).value()) * 100;
+                System.out.println(resulstsData.get(i).name() + " - " + prob +"%");
+            }
         }
+    }
+
+    public byte[] convertImageToByte(Uri uri){
+        byte[] data = null;
+        try {
+            ContentResolver cr = getBaseContext().getContentResolver();
+            InputStream inputStream = cr.openInputStream(uri);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            data = baos.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 }
