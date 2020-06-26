214d9e3bdbc44d6a4514c5cfbd01d5db9f8a8308
Daniel Rodrigues <daniel.maestro97@gmail.com>
2020-06-26 03:39:20 +0100

    ListView Favorites Implemented

--- app/src/main/java/pt/ipleiria/estg/foodzam/fragments/FavoritesFragment.java
+++ app/src/main/java/pt/ipleiria/estg/foodzam/fragments/FavoritesFragment.java
@@ -1,22 +1,85 @@
 package pt.ipleiria.estg.foodzam.fragments;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import pt.ipleiria.estg.foodzam.R;
 
 public class FavoritesFragment extends Fragment {
 
-
+    ListView listView;
+    String mTitle[] = {"Facebook", "Whatsapp","Twitter","Instagram","Youtube"};
+    String mDescription[] = {"Facebook Description", "Whatsapp Description","Twitter Description","Instagram Description","Youtube Description"};
+    int images[] = {R.drawable.face,R.drawable.what,R.drawable.twitter,R.drawable.insta,R.drawable.youtube};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        View fragment_view = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        listView = fragment_view.findViewById(R.id.listViewFavorites);
+
+        //now create an adapter class
+        MyAdapter adapter = new MyAdapter(requireActivity(), mTitle,mDescription,images);
+        listView.setAdapter(adapter);
+
+
+
+        //now set item click on list view
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        return fragment_view;
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter (Context c, String title[], String description[], int imgs[]){
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) requireActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.imageFavorites);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDescription = row.findViewById(R.id.textView2);
+
+            //now set our resources on views
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+            return row;
+        }
     }
 }
